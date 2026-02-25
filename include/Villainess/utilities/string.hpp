8f30c6becb6a42bf0b59787852b800ca7dca5333
Lavion <ilavion4u@proton.me>
2026-02-25 02:51:55 +0100

    Implement cli and game loop (#1)
    * Implement game loop & commands
    * Add CI workflow for test build

---
+++ include/Villainess/utilities/string.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+std::string to_lower(std::string_view str);
+std::string to_upper(std::string_view str);
+std::string to_trim(std::string_view str);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
+#include "game.hpp"
+#include "cli.hpp"
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from FG25GP_ZorkClone!\n";
+int main() {
+    Game game;
+    setupGame(game);
+    startGame(game);
+
+    std::string input;
+    while (game.isRunning) {
+        std::cout << "> ";
+        if (!std::getline(std::cin, input)) break;
+        if (input.empty()) continue;
+        procInput(game, input);
+    }
+
+    return 0;
 }
